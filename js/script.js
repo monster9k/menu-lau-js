f96b155adc75d74ce7f3e82731f600b749af674b
monster9k <monster722006@gmail.com>
2025-04-22 10:39:34 +0700

    add image and set api and compine it to HTML

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 let cart = [];
 
-function addtoCart(item, price) {}
+fetch("data/menu_lau.json") // Không dùng ../
+  .then((res) => res.json()) // nhớ parse JSON
+  .then((menu) => {
+    const container = document.querySelector(".menu-list");
+
+    container.innerHTML = "";
+
+    menu.forEach((item) => {
+      const HTML = `
+        <div class="item">
+            <h3>${item.name}</h3>
+            <img
+              src="${item.image}"
+              alt="${item.name}"
+              class="image_hotpot"
+            />
+            <p>${item.note}</p>
+            <p>Giá: ${item.price.toLocaleString()}</p>
+            <button>Add to cart</button>
+          </div>
+        `;
+
+      container.innerHTML += HTML;
+    });
+  })
+  .catch((error) => {
+    console.log("Lỗi khi tải file JSON:", error);
+  });
